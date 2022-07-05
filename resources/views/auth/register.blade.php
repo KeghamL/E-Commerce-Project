7c8523ae3a3475666b13095e3e68abe0ebca0862
Erik <gevorgyane699@gmail.com>
2022-07-05 12:48:07 +0400

    fixingg

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -35,7 +36,7 @@
                 <hr>
             </div>
             <form action="{{ route('register-user') }}" method="POST">
-                {{ csrf_field() }}
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" />
                 @if ($message = Session::get('success'))
                     <div class="alert alert-success">
                         <p>{{ $message }}</p>
