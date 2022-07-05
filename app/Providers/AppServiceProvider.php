971fc883539e9c110b6ae8ea0e195b1ecd577bd6
Erik <gevorgyane699@gmail.com>
2022-07-05 13:13:22 +0400

    fixingg

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        if (app()->environment('remote')) {
+        if (env('APP_ENV') === 'production') {
             URL::forceScheme('https');
         }
     }
