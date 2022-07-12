1818e880f0a2e44026b1d21670ed0bb73454bcd0
Erik <gevorgyane699@gmail.com>
2022-07-12 17:29:44 +0400

    fixingg

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -53,7 +53,7 @@ class ProductController extends Controller
         $request->validate([
             'name' => 'required',
             'description' => 'required',
-            'price' => 'required|numeric|min:3|max:5',
+            'price' => 'required|numeric|min:100|max:5000',
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
         // $data = $request->all();
