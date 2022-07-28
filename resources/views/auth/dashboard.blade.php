df35bf8c45867d9ded9474567419b5f836529513
Erik <gevorgyane699@gmail.com>
2022-07-28 12:57:22 +0400

    fixingg

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -88,7 +88,7 @@
                                                 class="fa fa-eye" aria-hidden="true"></i></a></li>
 
 
-                                    <li><button data-tip="Delete"><i class="fa fa-trash" aria-hidden="true"></i>
+                                    <li><button data-tip="Delete" type="submit"><i class="fa fa-trash" aria-hidden="true"></i>
                                     </li>
                                 </form>
                             @endif
